45f9b71d1a299337b07ec84f437f62176d091f0b
Larisa Bainton <larisabainton@gmail.com>
2023-09-22 18:11:30 -0400

    add fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,21 @@ module.exports = {
     title: `Lessons with Larisa`,
     siteUrl: `https://www.lessonswithlarisa.com`,
   },
-  plugins: ["gatsby-plugin-sass"]
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-omni-font-loader",
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        web: [{
+          name: "DM Serif Display",
+          file: 'https://fonts.googleapis.com/css2?family=DM+Serif+Display&display=swap'
+        },{
+          name: "Darker Grotesque",
+          file: 'https://fonts.googleapis.com/css2?family=Darker+Grotesque&display=swap'
+        }]
+      }
+      
+    }]
 };

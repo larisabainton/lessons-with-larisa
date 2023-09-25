fcface93ead3ffd1229fac53243346068c8fd658
Larisa Bainton <larisabainton@gmail.com>
2023-09-25 13:23:43 -0400

    style contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,24 +6,11 @@ const Contact = () => {
             <div className="contact_title">Contact Me</div>
             <div className="contact_form">
                 <form method="post" action="https://getform.io/f/ad45fa58-d5f5-4bcf-88ec-79299a0bf6eb">
-                <label>
-                    Name
-                    <input type="text" name="name" id="name" />
-                </label>
-                <label>
-                    Email
-                    <input type="email" name="email" id="email" />
-                </label>
-                <label>
-                    Subject
-                    <input type="text" name="subject" id="subject" />
-                </label>
-                <label>
-                    Message
-                    <textarea name="message" id="message" rows="5" />
-                </label>
-                <button type="submit">Send</button>
-                <input type="reset" value="Clear" />
+                    <input type="text" aria-label="name" name="name" id="name" placeholder="Name" />
+                    <input type="email" aria-label="email" name="email" id="email" placeholder="Email" />
+                    <input type="text" aria-label="subject" name="subject" id="subject" placeholder="Subject" />
+                    <textarea aria-label="message" name="message" id="message" rows="5" placeholder="Type your message here..."/>
+                <button type="submit">Submit</button>
                 </form>
             </div>
         </div>

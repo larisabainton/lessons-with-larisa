d9c99ed913bb8f572d4032caa7d8b6a2e304cb81
Larisa Bainton <larisabainton@gmail.com>
2023-10-04 09:29:42 -0400

    new cover photo

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -4,7 +4,7 @@ import { StaticImage } from 'gatsby-plugin-image';
 const Cover = () => {
     return (
         <div className="cover">
-            <StaticImage className="cover_image"src="../images/cover.png" alt="Room with instruments"/>
+            <StaticImage className="cover_image"src="../images/cover.jpg" alt="Room with instruments"/>
             <div className="cover_title">Voice, Piano, and Guitar Lessons</div>
         </div>
     )

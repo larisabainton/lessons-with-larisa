3d1500bd7cc8488ad964a5365c38e6d0ea8bbb67
Larisa Bainton <larisabainton@gmail.com>
2023-10-06 19:15:19 -0400

    try to fix production

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -4,7 +4,9 @@ import { StaticImage } from 'gatsby-plugin-image';
 const Cover = () => {
     return (
         <div className="cover">
-            <StaticImage className="cover_image"src="../images/cover.jpg" alt="Room with instruments"/>
+            <div className="cover_background">
+                <StaticImage className="cover_image"src="../images/cover.jpg" alt="Room with instruments"/>
+            </div>
             <div className="cover_title">Voice, Piano, and Guitar Lessons</div>
         </div>
     )

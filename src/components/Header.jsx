63fbade92d8be8ffbd0334c0a224af9deb1a4e53
Larisa Bainton <larisabainton@gmail.com>
2026-04-27 13:29:55 -0400

    added hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,32 @@ import { StaticImage } from 'gatsby-plugin-image';
 import { Link } from 'gatsby';
 import BookingButton from "./bookingButton";
 
+const pageLinks = (className) => {
+    return (
+        <div className={`${className} header_link-wrapper`}>
+            <Link to="/signup" className="header_link">Sign Up</Link>
+            <Link to="/login" className="header_link">Student Portal</Link>
+            <BookingButton />
+        </div>
+    )
+}
+
 const Header = () => {
     return (
         <div className="header">
             <div className="header-logo">
                 <Link to="/"><StaticImage className="header-title"src="../images/title.png" alt="Lessons with Larisa"/></Link>
             </div>
-            <div className="header_link-wrapper">
-                <Link to="/signup" className="header_link">Sign Up</Link>
-                <Link to="/login" className="header_link">Student Portal</Link>
-                <BookingButton />
+
+            {/* mobile */}
+            <div className="header_menu">
+                <StaticImage alt="" placeholder="none" className="menu_image" src="../images/menu.svg" />
+                {pageLinks("header_menu-links")}
             </div>
+
+            {/* desktop */}
+            {pageLinks("header_desktop-links")}
+
         </div>
     )
 };

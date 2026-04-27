a24dcc9118bff0e9dde0a7bb0661b2f564e309c1
Larisa Bainton <larisabainton@gmail.com>
2026-04-26 20:35:20 -0400

    change about text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
 
-const paragraph = "Larisa is a soprano and voice teacher who has been working in the Boston area since 2016. In 2020 she completed a Masters Degree in Vocal Pedagogy at the New England Conservatory (NEC). She has worked as an instructor at the Eclectic School of Music (Atlanta, GA), as a teaching assistant at NEC, and at the Amy Dancz Studio (Boston, MA). Her in-person private studio is located in Malden, MA."
+const paragraph = "Larisa is a pianist, guitarist, and classically trained soprano who has been performing in the greater Boston area for over a decade. She completed the dual degree program at the University of Rochester and Eastman School of Music, receiving a Bachelor of Arts in Brain & Cognitive Sciences, and a Bachelor of Music in Vocal Performance. Larisa received her Master of Music in Vocal Pedagogy from New England Conservatory. As part of her continuing education she completed a certificate in Accessible Music Education through the Berklee College of Music."
 
 const About = () => {
     return (
@@ -10,7 +10,7 @@ const About = () => {
                 <StaticImage src="../images/headshot.jpg" alt="Larisa Bainton headshot" />
             </div>
             <div className="about_text">
-                <div className="about_text_title">About Me</div>
+                <div className="about_text_title">About</div>
                 <div className="about_text_paragraph">{paragraph}</div>
             </div>
         </div>
